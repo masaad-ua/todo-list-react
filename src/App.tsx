9f1768694a0045d54c0a11741811070412cc6944
Maksym Osypchuk <maksym3d@gmail.com>
2026-05-29 12:13:49 +0300

    update( App.module.scss, App.tsx ): add style for spinner, update functions loadTasks, addItem,deleteItem.

--- src/App.tsx
+++ src/App.tsx
@@ -7,43 +7,77 @@ import {createTask, deleteTask, getTasks, type TaskI} from "./shared/tasksApi";
 function App() {
     const [items, setItems] = useState<TaskI[]>([]);
     const [value, setValue] = useState("");
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        getTasks()
-            .then(setItems)
-            .catch(console.error)
+
+        const loadTasks = async () => {
+            try{
+                setLoading(true)
+                const tasks = await getTasks();
+                setItems(tasks);
+            }
+            catch (error){
+                console.error(error);
+            }finally {
+                setLoading(false);
+            }
+        }
+
+        loadTasks();
     }, []);
 
     const addItem = async () => {
-        if(!value.trim()){
-            alert("Please write your todo task");
-            return;
+        try {
+            setLoading(true);
+
+            if(!value.trim()){
+                alert("Please write your todo task");
+                return;
+            }
+
+            const  newItem: TaskI = await createTask(value);
+            setItems([...items, newItem]);
+            setValue("");
+        } finally {
+            setLoading(false);
         }
-        const  newItem: TaskI = await createTask(value);
-        setItems([...items, newItem]);
-        setValue("");
+
     }
 
     const deleteItem = async (id: number) => {
-        await deleteTask(id);
+        try {
+            setLoading(true);
+            await deleteTask(id);
 
-        const newItems = items.filter((item: TaskI)=> {
-            return item.id !== id
-        })
-        setItems([ ...newItems])
+            const newItems = items.filter((item: TaskI)=> {
+                return item.id !== id
+            })
+            setItems([ ...newItems])
+        }
+        finally {
+            setLoading(false);
+        }
     }
 
     return (
         <div className={`${cls.app_container}`}>
             <div className={`${cls.todo_container}`} >
                 <h1 className={`${cls.h1_title}`}>Todo App</h1>
+
                 <div className={"flexible column"}>
+
                     <TodoInput
                         value = {value}
                         onChange = { setValue }
                         addItem = { addItem }
                     />
                     <div className={"flexible column"}>
+                        {loading && (
+                            <div className={`${cls.spinner_container} flexible justify-center`}>
+                                <div className={cls.spinner}></div>
+                            </div>
+                        )}
                         {items.map((item: TaskI) => (
                                 <TodoItem
                                     key={item.id}
